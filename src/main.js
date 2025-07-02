249ec0b2c006aba7c21a26118d812144f7ced772
Santino <santinogarronegg@gmail.com>
2025-07-02 19:36:29 -0300

    Arreglando errores y perfeccionando

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ const selectOrden = document.getElementById("ordenar-options");
 const barraBusqueda = document.getElementById("search-bar");
 let estadoActual = "";
 let ordenActual = "az";
-
+import "./style.css";
 // esta función obtiene los personajes de la API de Rick y Morty segun la pagina,
 // y los muestra como cards. si hay algún error muestra un mensaje.
 
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/", // Si el proyecto se despliega en el root del dominio
+});
